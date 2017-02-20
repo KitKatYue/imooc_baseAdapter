000887dc076fcc8d2898daff819a1e7d48d8e81c
Administrator <18056684432@qq.com>
2017-02-21 04:08:07 +0800

    第一次提交

---
+++ app/src/main/java/com/example/bean/Bean.java
@@ -0,0 +1,54 @@
+package com.example.bean;
+
+/**
+ * Created by SanYue
+ * 2017/2/21
+ * email sanyuedev@gmail.com
+ */
+
+public class Bean {
+
+    private String title;
+    private String desc;
+    private String time;
+    private String phone;
+
+    public Bean(String title, String desc, String time, String phone) {
+        this.title = title;
+        this.desc = desc;
+        this.time = time;
+        this.phone = phone;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
